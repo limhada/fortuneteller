2f0b21e80a8df377793508422e13f879301cb629
limhada <leem1315@gmail.com>
2023-05-05 15:17:26 +0900

    ✨ 요청 실패시 다시 요청하는 로직 추가

--- frontend/script.js
+++ frontend/script.js
@@ -73,7 +73,7 @@ function sendMessage() {
   if (message) {
     // 입력된 메시지가 있으면
     addMessage(message, true); // 사용자의 메시지로 간주하여 채팅 박스에 추가
-    getFortune(message); // 입력된 메시지를 이용해 챗봇으로부터 운세를 받아옴
+    getFortune(); // 입력된 메시지를 이용해 챗봇으로부터 운세를 받아옴
     inputBox.value = ""; // 입력 상자를 초기화
 
     loading();
@@ -89,8 +89,16 @@ function loading() {
   document.getElementById("btn").style.display = "none";
 }
 
+
+function sleep(sec) {
+  return new Promise(resolve => setTimeout(resolve, sec * 1000));
+}
+
 // 챗봇에게 운세를 요청하는 함수
-async function getFortune(message) {
+async function getFortune() {
+  const maxRetries = 3;
+  let retries = 0;
+  while (retries < maxRetries) {
   try {
     const response = await fetch(
       "https://p9vt8sq7sj.execute-api.ap-northeast-2.amazonaws.com/prod/fortuneTell",
@@ -123,8 +131,15 @@ async function getFortune(message) {
     return data; // 받은 데이터를 반환
   } catch (error) {
     // 오류 발생 시 콘솔에 출력
+    await sleep(0.5);
+    retries++;
     console.log(error);
+    console.log(`Error fetching data, retrying (${retries}/${maxRetries})...`);
+    if (retries === 3) {
+      alert("서버가 불안정합니다. 잠시 후 다시 시도해주세요!");
+    }
   }
+}
 }
 
 document
